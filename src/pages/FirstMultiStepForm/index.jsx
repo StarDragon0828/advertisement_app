c98d3076ad0206fcf81dc46043b2fa9a5be1f650
Venkatesh <venkatesh.a@eaglemountsoft.com>
2023-12-07 21:21:05 +0530

    part eight added

--- src/pages/FirstMultiStepForm/index.jsx
+++ src/pages/FirstMultiStepForm/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Layout from "../../Layout/index";
 import {
-    Box,
-    Container,
-    Typography,
-    Grid,
-    IconButton,
-    LinearProgress,
+  Box,
+  Container,
+  Typography,
+  Grid,
+  IconButton,
+  LinearProgress,
 } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import PartOne from "./Stages/firstStep/PartOne";
@@ -17,76 +17,102 @@ import PartFour from "./Stages/fourthStep/PartFour";
 import PartFive from "./Stages/fiveStep/PartFive";
 import PartSix from "./Stages/sixStep/PartSix";
 import PartSeven from "./Stages/sevenStep/PartSeven";
+import PartEight from "./Stages/eightStep/PartEight";
 
 const FirstMultiStepForm = () => {
-    const [activeStage, setActiveStage] = useState(1);
-    const totalStages = 7; // Update this based on the total number of steps
+  const [activeStage, setActiveStage] = useState(8);
+  const totalStages = 8; // Update this based on the total number of steps
 
-    const handleStageChanges = (updatedStage) => {
-        setActiveStage(updatedStage);
-    };
+  const handleStageChanges = (updatedStage) => {
+    setActiveStage(updatedStage);
+  };
 
-    const progressValue = (activeStage / totalStages) * 100;
+  const progressValue = (activeStage / totalStages) * 100;
 
-    const stepTitles = {
-        1: 'Criar Negocio',
-        2: 'Contato',
-        3: 'Local e Área',
-        4: 'Horário',
-        5: 'Membros',
-        6: 'Dados Bancários',
-        7: 'Complemento'
-        // ... Add more titles for additional steps
-    };
+  const stepTitles = {
+    1: "Criar Negocio",
+    2: "Contato",
+    3: "Local e Área",
+    4: "Horário",
+    5: "Membros",
+    6: "Dados Bancários",
+    7: "Complemento",
+    8: "Criar Anúncio",
+    // ... Add more titles for additional steps
+  };
 
-    return (
-        <Layout>
-            <Container maxWidth={"md"} sx={{ bgcolor: "background.main", boxShadow: 1, borderRadius: 2, padding: 2, marginTop: 7, marginBottom: 10, }}>
-                {/* Common Header */}
-                <Grid container spacing={1}>
-                    <div className="step-header">
-                        <Grid item xs={1}>
-                            <IconButton color="primary" aria-label="back" onClick={() => handleStageChanges(activeStage - 1)} disabled={activeStage === 1}>
-                                <ArrowBackIosIcon />
-                            </IconButton>
-                        </Grid>
-                        <Grid item xs={11}  >
-                            <Typography variant="h3" color="text.primary" component="h2" align="center" className="step-title">
-                                {stepTitles[activeStage]}
-                            </Typography>
-                        </Grid>
-                    </div>
-                    <Grid item xs={12}>
-                        {/* Progress bar */}
-                        <CustomizedProgressBars currentStep={progressValue} />
-                    </Grid>
-                </Grid>
-                {/* Step Content */}
-                {activeStage === 1 && (
-                    <PartOne handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 2 && (
-                    <PartTwo handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 3 && (
-                    <PartThree handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 4 && (
-                    <PartFour handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 5 && (
-                    <PartFive handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 6 && (
-                    <PartSix handleStageChanges={handleStageChanges} />
-                )}
-                {activeStage === 7 && (
-                    <PartSeven handleStageChanges={handleStageChanges} />
-                )}
-                {/* ... other steps */}
-            </Container>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Container
+        maxWidth={"md"}
+        sx={{
+          bgcolor: "background.main",
+          boxShadow: 1,
+          borderRadius: 2,
+          padding: 2,
+          marginTop: 7,
+          marginBottom: 10,
+        }}
+      >
+        {/* Common Header */}
+        <Grid container spacing={1}>
+          <div className="step-header">
+            <Grid item xs={1}>
+              <IconButton
+                color="primary"
+                aria-label="back"
+                onClick={() => handleStageChanges(activeStage - 1)}
+                disabled={activeStage === 1}
+              >
+                <ArrowBackIosIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={11}>
+              <Typography
+                variant="h3"
+                color="text.primary"
+                component="h2"
+                align="center"
+                className="step-title"
+              >
+                {stepTitles[activeStage]}
+              </Typography>
+            </Grid>
+          </div>
+          <Grid item xs={12}>
+            {/* Progress bar */}
+            <CustomizedProgressBars currentStep={progressValue} />
+          </Grid>
+        </Grid>
+        {/* Step Content */}
+        {activeStage === 1 && (
+          <PartOne handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 2 && (
+          <PartTwo handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 3 && (
+          <PartThree handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 4 && (
+          <PartFour handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 5 && (
+          <PartFive handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 6 && (
+          <PartSix handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 7 && (
+          <PartSeven handleStageChanges={handleStageChanges} />
+        )}
+        {activeStage === 8 && (
+          <PartEight handleStageChanges={handleStageChanges} />
+        )}
+        {/* ... other steps */}
+      </Container>
+    </Layout>
+  );
 };
 
 export default FirstMultiStepForm;
